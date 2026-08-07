ee5e035cf9664492c7c62d186cf43aa32585b01b
Vanshika <vanshika.sh26@gmail.com>
2026-08-07 11:42:31 +0530

    Day 1: Variables, input, f-strings - first scripts

---
+++ Day1.py
@@ -0,0 +1,16 @@
+#ques = input("Apna sawaal yahan likho: ")
+name = input("Apna naam batao: ")
+age = input("Apni age batao: ")
+food = input("Apna favourite food batao: ")
+course = input("Apna course bta:")
+University = input("University bta :")
+numberofprojects = input("bta:")
+#f ka meaning h formatted string
+# {} ka meaning h jo variable {} h usko replace krna h 
+print(f"Hello {name}! 👋")
+print(f"{age} saal ki age mein automation seekhna - respect! 🔥")
+print(f"Aur haan, {food} khate khate code karna best hota hai 😄")
+
+#self
+print(f"Hello Sir !! My name is {name} . I am currently persuing {course} from {University}. I build {numberofprojects} projcts that enhance my resume.") 
+
