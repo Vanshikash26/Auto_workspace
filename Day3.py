66d6903b4ef3ab3c9b55703501195c1b1d46ad46
Vanshika <vanshika.sh26@gmail.com>
2026-08-08 10:42:32 +0530

    Day 3: If-Else

---
+++ Day3.py
@@ -0,0 +1,9 @@
+#trial
+age = int(input("Apni age batao: "))
+
+if age >= 18:
+    print("You are eligible for voting! 🗳️")
+elif age<=0:
+    print("Funny")
+else:
+    print("Wait for sometime ⏳")    
